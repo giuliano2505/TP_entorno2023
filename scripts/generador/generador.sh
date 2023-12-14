9a45668618826a0ed193db7e5b8da64b1a0d22e1
ValentinaB <valentinabalverdim@gmail.com>
2023-12-14 18:40:55 -0300

    modificacion generador.sh

--- scripts/generador/generador.sh
+++ scripts/generador/generador.sh
@@ -1,5 +1,42 @@
 #!/bin/bash
 
 echo "GENERADOR FUNCIONANDO"
+# Función para generar archivos de texto
+generate_text_file() {
+    base64 /dev/urandom | head -c 500 > "$1"
+}
 
-exit 0
+# Función para generar archivos de sonido
+generate_sound_file() {
+    ffmpeg -f lavfi -i "anoisesrc=a=0.1:c=white:duration=5" "$1"
+}
+
+# Función para generar archivos de imagen
+generate_image_file() {
+    convert -size 100x100 xc: +noise Random "$1"
+}
+
+# Generar un número aleatorio para decidir qué tipo de archivo crear
+random_number=$((RANDOM % 3))
+
+# Nombre del archivo
+output_file="output_file"
+
+# Dependiendo del número aleatorio, llamar a la función correspondiente
+case $random_number in
+    0)
+        generate_text_file "${output_file}.txt"
+        echo "Se ha generado un archivo de texto: ${output_file}.txt"
+        ;;
+    1)
+        generate_sound_file "${output_file}.wav"
+        echo "Se ha generado un archivo de sonido: ${output_file}.wav"
+        ;;
+    2)
+        generate_image_file "${output_file}.png"
+        echo "Se ha generado un archivo de imagen: ${output_file}.png"
+        ;;
+    *)
+        echo "Número aleatorio fuera de rango."
+        ;;
+esac
