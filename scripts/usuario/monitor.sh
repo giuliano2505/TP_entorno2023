0ae38888879b1ac04a35f27287e8a1017f1f52e5
Giuliano Carlachiani <giuliano2505@gmail.com>
2023-12-18 00:52:55 -0300

    Agrego uso de RAM

--- scripts/usuario/monitor.sh
+++ scripts/usuario/monitor.sh
@@ -9,4 +9,13 @@ TIMESTAMP=$(date +"%d_%m_%Y:%H_%M_%S")
 #Imprimimos el segundo campo de la linea
 USOCPU=$(top -b -n 1 | grep "%Cpu(s)" | awk '{print $2}')
 
+#Uso de memoria
+#Se podria hacer con top tambien
+#Llamamos a freey capturamos la linea de memoria
+#Tomamos el tercer campo(mem en uso), lo dividimos por el segundo (memtotal)y lo multiplicamos por 100
+USORAM=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
+
+
+
+
 exit 0
