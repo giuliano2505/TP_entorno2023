228fab25fdf84cdf04649227b8c2182b817c36f6
Agustina <sandaishusui@gmail.com>
2023-12-21 19:05:57 -0300

    Se cambió el nombre del archivo y se movió

---
+++ scripts/usuario/clasificador.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Directorio de origen
+
+origen="/app/inputs"
+
+DIRSALIDA="/app/outputs"
+
+
+DIRIMAGENES="$DIRSALIDA/img"
+
+mkdir -p "$DIRIMAGENES"
+
+DIRTEXTO="$DIRSALIDA/txt"
+
+mkdir -p "$DIRTEXTO"
+
+DIRSONIDO="$DIRSALIDA/snd"
+
+mkdir -p "$DIRSONIDO"
+
+
+
