a4ca51dd7c3376301dd787b982339f4a8a44bb4f
Giuliano Carlachiani <giuliano2505@gmail.com>
2023-12-23 12:01:40 -0300

    Arreglo formato

--- scripts/usuario/clasificador.sh
+++ scripts/usuario/clasificador.sh
@@ -9,6 +9,8 @@ CARPETASALIDA="/app/outputs/"
 mkdir -p "$CARPETASALIDA/img"
 mkdir -p "$CARPETASALIDA/snd"
 mkdir -p "$CARPETASALIDA/txt"
+chmod 777 "$CARPETASALIDA/img" "$CARPETASALIDA/snd" "$CARPETASALIDA/txt" 
+
 
 
 # Iterar sobre los archivos en la carpeta de entrada
@@ -24,25 +26,29 @@ do
     #DEBERIAMOS ELEGIR img/imagen , snd/sonido , txt/texto
     if echo "$tipo_archivo" | grep -q "text"; then
         SUBCARPETA="txt/texto"
+        EXTENSION=".txt"
     elif echo "$tipo_archivo" | grep -q "image"; then
         SUBCARPETA="img/imagen"
+        EXTENSION=".png"
     elif echo "$tipo_archivo" | grep -q "audio"; then
         SUBCARPETA="snd/sonido"
+        EXTENSION=".wav"
     else
         continue
     fi
 
     # Construir el nuevo nombre con la ruta de salida
-    RUTASALIDA="$CARPETASALIDA$TIPO$CONTADOR.png"
+    RUTASALIDA="$CARPETASALIDA$SUBCARPETA$TIPO$CONTADOR.$EXTENSION"
 
     # Verificar si el nuevo nombre ya existe
     while [ -e "$RUTASALIDA" ]
     do
       ((CONTADOR++))
-      RUTASALIDA="$CARPETASALIDA$SUBCARPETA$TIPO$CONTADOR.png"
+      RUTASALIDA="$CARPETASALIDA$SUBCARPETA$TIPO$CONTADOR.$EXTENSION"
     done
 
     mv "$ARCHIVO" "$RUTASALIDA"
     
   fi
 done
+echo -e "Archivos clasificados\n"
